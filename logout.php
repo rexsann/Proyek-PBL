aae38b160e132e95a6030ac841b82cd533b23a25
rexsann <m.ihsan2106@gmail.com>
2025-12-23 20:59:49 +0700

    update lgout

--- logout.php
+++ logout.php
@@ -1,8 +1,15 @@
 <?php
 session_start();
+
+// Hapus semua data session
+$_SESSION = [];
 session_unset();
 session_destroy();
 
+// Buat session baru khusus untuk notifikasi logout
+session_start();
+$_SESSION['logout_success'] = "Anda berhasil logout.";
+
+// Arahkan ke halaman login atau home
 header("Location: login.php");
-exit()
-?>
+exit;
